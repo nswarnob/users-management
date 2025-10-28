d0f8f137b51fe236dbc19e3a41ff930337511441
Sharif Uddin Arnob <nswarnob@gmail.com>
2025-10-29 03:11:55 +0600

    users added to the ui

--- users-management-client/src/App.jsx
+++ users-management-client/src/App.jsx
@@ -1,18 +1,17 @@
+import "./App.css";
+import Users from "./components/Users";
 
-import './App.css'
-import Users from './components/Users';
-
-
-const usersPromise = fetch('http://localhost:3000/users').then(res=>res.json());  
+const usersPromise = fetch("http://localhost:3000/users").then((res) =>
+  res.json()
+);
 
 function App() {
   return (
     <>
-      
-      <h1>Users Manangement</h1>
+      <h1>Users Management</h1>
       <Users usersPromise={usersPromise}></Users>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- users-management-client/src/components/Users.jsx
+++ users-management-client/src/components/Users.jsx
@@ -1,52 +1,75 @@
-import React, { useState } from 'react';
-import { use } from 'react';
+import React, { useState } from "react";
+import { use } from "react";
 
-const Users = ({usersPromise}) => {
- 
-    const initialUsers = use(usersPromise);
-    const [users, setUsers] = useState(initialUsers);
-    
-    const handleAddUser = (e) => {
-        e.preventDefault();
-        const name = e.target.name.value;
-        const email = e.target.email.value;
+const Users = ({ usersPromise }) => {
+  const initialUsers = use(usersPromise);
+  const [users, setUsers] = useState(initialUsers);
 
-        // For simplicity, we are not sending the user data in the request body
-        fetch('http://localhost:3000/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name, email }),
-        })
-        .then(res => res.json())
-        .then(data=>{
-            console.log("User added:", data);
-            const updatedUsers = [...users, data];
-            setUsers(updatedUsers);
-            e.target.reset();
-        })
-    }
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
 
-    return (
-        <div>
-            <div>
-                <h1>Add a user</h1>
-                <form onSubmit={handleAddUser} >
-                <input type="text" name='name'/>
-                <br />
-                <input type="email" name='email' />
-                <br />
-                <button>Add User</button>
-                </form>
-            </div>
-           <div>
-             {
-                users.map(user=> <p key={user.id} >{user.name} - {user.email}</p>)
-            }
-           </div>
-        </div>
-    );
+    // For simplicity, we are not sending the user data in the request body
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("User added:", data);
+        const updatedUsers = [...users, data];
+        setUsers(updatedUsers);
+        e.target.reset();
+      })
+      .catch((error) => {
+        console.error("Error adding user:", error);
+        alert("Failed to add user. Please try again.");
+      });
+  };
+  return (
+    <div>
+      <div>
+        <h1>Add a user</h1>
+        <form onSubmit={handleAddUser}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            placeholder="Enter name"
+          />
+          <br />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+          <br />
+          <button>Add User</button>
+        </form>{" "}
+      </div>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} - {user.email}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
